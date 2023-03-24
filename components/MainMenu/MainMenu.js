4a5fa79116cff98d69b366b9dc5713cfbf12a030
eelcoverbrugge <hallo@eelcoverbrugge.nl>
2023-03-24 18:18:25 +0100

    Revert "change navbar to responsive navbar"
    This reverts commit 57bc26b94a22e7bfd4544ad5beae38b7b0e2a6e0.

--- components/MainMenu/MainMenu.js
+++ components/MainMenu/MainMenu.js
@@ -11,17 +11,6 @@ export const MainMenu = ({ items, callToActionLabel, callToActionDestination }) 
   const [backgroundTransparancy, setBackgroundTransparancy] = useState(0);
   const [boxShadow, setBoxShadow] = useState(0);
 
-  const [mobileMenu, setMobileMenu] = useState(false);
-  const [menuItem, setMenuIte,] = useState(false);
-
-  const toggleMobileMenu = () => {
-    setMobileMenu(!mobileMenu);
-  }
-
-  const toggleMenuItem = () => {
-    setMenuIte(!menuItem);
-  }
-
   useEffect(() => {
     let backgroundTransparancyVar = clientWindowHeight / 600; //1000/600=1.6
 
@@ -38,11 +27,17 @@ export const MainMenu = ({ items, callToActionLabel, callToActionDestination }) 
 
   const handleScroll = () => {
     setClientWindowHeight(window.scrollY);
-  };
+  }
 
   return (
-    <nav className="sticky flex top-1 bg-gradient-to-r from-rose-500 to-pink-500">
-      <div className="flex items-center p-2 gap-2">
+    <div
+      className="text-white px-5 sticky top-0 z-20 flex items-center"
+         style={{
+           background: `rgba(41, 34, 56, ${backgroundTransparancy})`,
+           boxShadow: `rgb(0 0 0 / ${boxShadow}) 0px 0px 20px 6px`,
+         }}
+    >
+      <div className="py-4 pl-5 flex text-pink-600">
         <Link href="/">
           <Image src={WhcLogo}
                  height="45"
@@ -50,76 +45,32 @@ export const MainMenu = ({ items, callToActionLabel, callToActionDestination }) 
                  alt="logo"
           />
         </Link>
-        <div className="font-bold text-2xl">
-          tw:<span className="text-sky-900">mf</span>
-        </div>
       </div>
-
-
-      <div className="block md:hidden ml-auto pr-4 my-auto cursor-pointer">
-        <div id="mobile-menu-button" onClick={toggleMobileMenu} className={`group peer ${toggleMobileMenu && 'open'}`}>
-          <div
-            className="top-0 bg-zinc-200 rounded-full w-8 h-1 group-open:rotate-45 transition-all group-open:top-2 relative"></div>
-          <div className="bg-zinc-200 rounded-full w-8 h-1 mt-1 opacity-100 group-open:opacity-0 transition-all"></div>
-          <div
-            className="top-0 bg-zinc-200 rounded-full w-8 h-1 mt-1 group-open:-rotate-45 transition-all group-open:-top-2 relative"></div>
-        </div>
-
-        <div
-          className="absolute bg-gradient-to-r from-rose-500 to-pink-500 w-full left-0 top-[62px] peer-open:block hidden">
-          {(items || []).map(item => (
-            <div key={item.id}>
-              {!!item.subMenuItems?.length ? (
-                <div id="ticket-menu-item"
-                     onClick={toggleMenuItem}
-                     className={`group ${toggleMenuItem && 'open'} relative h-full cursor-pointer text-pink-200 hover:text-zinc-200 transition-colors hover:bg-white/10`}>
-                  <div className="p-4 text-center font-bold ">{item.label}</div>
-                  <div className="hidden group-open:block">
-                    {item.subMenuItems.map(subMenuItem => (
-                      <div key={subMenuItem.id}
-                           className="text-center p-4 relative text-pink-200 hover:text-zinc-200 hover:bg-white/5 transition-colors ease-in-out">
-                        <span>{subMenuItem.label}</span>
-                      </div>
-                    ))}
-                  </div>
-                </div>
-              ) : (
-                <div key={item.id}
-                className="relative flex h-full cursor-pointer items-center justify-center p-4 font-bold text-pink-200 hover:text-zinc-200 transition-colors hover:bg-white/10">
-                <span>{item.label}</span>
-                </div>
-                )}
-            </div>
-          ))}
-
-        </div>
-      </div>
-      <div className="hidden flex-1 md:flex items-center justify-end">
+      <div className="flex flex-1 justify-end">
         {(items || []).map(item => (
-          <div key={item.id}>
-            {!!item.subMenuItems?.length ? (
-              <div className="menu-item group">
-                <span>{item.label}</span>
-                <div
-                  className="group-hover:block hidden absolute top-full bg-pink-500 right-0 whitespace-nowrap rounded-b-md text-right p-3">
+          <div key={item.id} className="hover:bg-slate-700 cursor-pointer relative group">
+            <div>
+              <Link href={item.destination}>
+                <a className="p-5 block">{item.label}</a>
+              </Link>
+              {!!item.subMenuItems?.length && (
+                <div className="group-hover:block hidden bg-slate-800 text-right absolute right-0 top-full -mt-3">
                   {item.subMenuItems.map(subMenuItem => (
-                    <div key={subMenuItem.id}>
-                      <div
-                        className="p-4 font-bold hover:bg-white/50 hover:text-zinc-200 transition-colors ease-in-out cursor-pointer text-pink-200">
-                        <span>{subMenuItem.label}</span>
-                      </div>
-                    </div>
+                    <Link key={subMenuItem.id} href={subMenuItem.destination}>
+                      <a className="block whitespace-nowrap p-5 hover:bg-slate-700">{subMenuItem.label}</a>
+                    </Link>
                   ))}
                 </div>
-              </div>
-            ) : (
-              <div className="menu-item">
-                <span>{item.label}</span>
-              </div>
-            )}
+              )}
+            </div>
           </div>
         ))}
+        <div className="ml-3 my-auto">
+          <ButtonLink destination={callToActionDestination}
+                      label={callToActionLabel}
+          />
+        </div>
       </div>
-    </nav>
+    </div>
   );
 };
