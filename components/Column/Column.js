feea33f5ab7babd1ed8d8de60a840a93c8f555d9
eelcoverbrugge <hallo@eelcoverbrugge.nl>
2023-03-25 17:26:03 +0100

    Move padding from inline styling to tailwind classes

--- components/Column/Column.js
+++ components/Column/Column.js
@@ -2,12 +2,33 @@ export const Column = ({ children, width, textColor, backgroundColor, padding, b
   const textColorStyle = textColor ? { color: textColor } : {};
   const backgroundColorStyle = backgroundColor ? { backgroundColor } : {};
   const widthStyle = width ? { minWidth: width, flexGrow: 1 } : { flexGrow: 1, flexBasis: 0 };
-  const paddingStyle = padding ? {
-    paddingTop: padding?.top ? padding?.top.match(/\d/g).join("") + "px" : "",
-    paddingRight: padding?.right ? padding?.right.match(/\d/g).join("") + "px" : "",
-    paddingBottom: padding?.bottom ? padding?.bottom.match(/\d/g).join("") + "px" : "",
-    paddingLeft: padding?.left ? padding?.left.match(/\d/g).join("") + "px" : ""
-  } : {};
+
+  console.log(padding)
+  const paddingTop = padding?.top ? padding?.top.match(/\d/g).join("")/10 : ""; //40
+  const paddingRight = padding?.right ? padding?.right.match(/\d/g).join("")/10 : ""; //40
+  const paddingBottom = padding?.bottom ? padding?.bottom.match(/\d/g).join("")/10 : ""; //40
+  const paddingLeft = padding?.left ? padding?.left.match(/\d/g).join("")/10 : ""; //40
+
+  let paddingT = Number(paddingTop) ? `pt-${paddingTop}` : "";
+  let paddingR = Number(paddingRight) ? `pr-${paddingRight}` : "";
+  let paddingB = Number(paddingBottom) ? `pb-${paddingBottom}` : "";
+  let paddingL = Number(paddingLeft) ? `pl-${paddingLeft}` : "";
+  let paddingY = "";
+  let paddingX = "";
+
+  if (paddingTop && paddingTop === paddingBottom) {
+    console.log(paddingTop)
+    paddingT = "";
+    paddingB = "";
+    paddingY = `py-${paddingTop}`;
+  }
+
+  if (paddingLeft && paddingLeft === paddingRight) {
+    paddingR = "";
+    paddingL = "";
+    paddingX = `px-${paddingLeft}`;
+  }
+
   const borderStyle = border ? {
     borderTop: border?.top ? "1px solid #292238" : "",
     borderRight: border?.right ? "1px solid #292238" : "",
@@ -20,9 +41,8 @@ export const Column = ({ children, width, textColor, backgroundColor, padding, b
       ...widthStyle,
       ...textColorStyle,
       ...backgroundColorStyle,
-      ...paddingStyle,
       ...borderStyle
-    }} className="px-2 py-5">
+    }} className={`px-2 py-5 ${paddingT} ${paddingR} ${paddingB} ${paddingL} ${paddingY} ${paddingX}`}>
       {children}
     </div>
   );
