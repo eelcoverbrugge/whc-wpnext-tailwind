bd2d77ecf71475ebe1e521272810b3b2d3338c2e
eelcoverbrugge <hallo@eelcoverbrugge.nl>
2023-03-25 14:53:06 +0100

    Add arrow to button

--- components/ButtonLink/ButtonLink.js
+++ components/ButtonLink/ButtonLink.js
@@ -1,12 +1,19 @@
 import Link from "next/link";
+import Image from "next/image";
+import ArrowRight from "../../assets/images/witte-linkbalk-pijl-rechts.png";
 
 export const ButtonLink = ({ destination, label }) => {
   return (
     <Link href={destination}>
-      <a
-        className="btn">
+      <button className="btn flex gap-5 items-center">
         {label}
-      </a>
+        <Image
+          src={ArrowRight}
+          alt="arrow to the right"
+          width="83"
+          height="20"
+        />
+      </button>
     </Link>
   );
 };
