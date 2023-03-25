8ff8393ecf5f6b1092acba7a28fb478860c7aad5
eelcoverbrugge <hallo@eelcoverbrugge.nl>
2023-03-25 14:56:21 +0100

    remove overlay

--- components/Cover/Cover.js
+++ components/Cover/Cover.js
@@ -4,7 +4,7 @@ import { usePageContext } from "../../context/page";
 export const Cover = ({ children, background }) => {
   const { featuredImage } = usePageContext();
   return (
-    <div className="h-[calc(100vh-100px)] min-h-[400px] bg-center bg-cover bg-slate-600 relative left-0 -top-[77px] flex justify-center items-center">
+    <div className="h-[calc(100vh-100px)] min-h-[400px] bg-center bg-cover relative left-0 -top-[77px] flex justify-center items-center">
       {(!!background || featuredImage) && (
         <Image alt="PostTitle" src={background || featuredImage} layout="fill" objectFit="cover"
                className="mix-blend-soft-light"></Image>
