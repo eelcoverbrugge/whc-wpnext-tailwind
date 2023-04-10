6e400d83bbb01a852f801f8ed5d02e1a9a99fe0c
Eelco Verbrugge <hallo@eelcoverbrugge.nl>
2023-04-10 17:40:27 +0200

    Change icon to download icon

--- components/TickItem/TickItem.js
+++ components/TickItem/TickItem.js
@@ -1,13 +1,12 @@
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCircleCheck } from "@fortawesome/free-solid-svg-icons";
+import { FaFileDownload } from "react-icons/fa";
 
 export const TickItem = ({ children }) => {
   return (
     <div className="grid grid-cols-[50px_1fr] gap-3">
       <div className="text-3xl text-green-500 flex justify-center">
-        <FontAwesomeIcon icon={faCircleCheck} />
+        <FaFileDownload size={20}/>
       </div>
-      <div>
+      <div className="hover:underline">
         {children}
       </div>
     </div>
