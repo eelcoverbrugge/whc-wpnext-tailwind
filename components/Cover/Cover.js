1920196b0dcf1640ebba871320f2cfdf92cb70af
Eelco Verbrugge <hallo@eelcoverbrugge.nl>
2023-04-10 17:40:49 +0200

    Polish code

--- components/Cover/Cover.js
+++ components/Cover/Cover.js
@@ -1,6 +1,5 @@
 import Image from "next/image";
 import { usePageContext } from "../../context/page";
-import { FaArrowDown } from "react-icons/fa";
 
 export const Cover = ({ children, background }) => {
   const { featuredImage } = usePageContext();
@@ -8,8 +7,13 @@ export const Cover = ({ children, background }) => {
     <>
       <div className="h-[calc(100vh-100px)] min-h-[400px] bg-center bg-cover relative left-0 -top-[77px] flex justify-center items-center">
         {(!!background || featuredImage) && (
-          <Image alt="PostTitle" src={background || featuredImage} layout="fill" objectFit="cover"
-                 className="mix-blend-soft-light" priority></Image>
+          <Image alt="PostTitle"
+                 src={background || featuredImage}
+                 layout="fill"
+                 objectFit="cover"
+                 className="mix-blend-soft-light"
+                 priority
+          />
         )}
         <div className="max-w-7xl z-10">
           {children}
