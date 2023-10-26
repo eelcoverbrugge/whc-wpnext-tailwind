ec2649d95415340954a03bf4c44e7697c9a096a7
eelcoverbrugge <hallo@eelcoverbrugge.nl>
2023-10-26 13:39:38 +0200

    [FEAT] add Google Analytics 4

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,11 @@
 import "../styles/globals.css";
 import {ParallaxProvider} from "react-scroll-parallax";
+import { GoogleAnalytics } from "nextjs-google-analytics";
 
 function MyApp({ Component, pageProps }) {
   return (
     <div className="font-body">
+      <GoogleAnalytics trackPageViews gaMeasurementId={"G-X63KM0K59M"} />
       <ParallaxProvider scrollAxis={"vertical"}>
         <Component {...pageProps} />
       </ParallaxProvider>
