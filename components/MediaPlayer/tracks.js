efccc7d3c3d0debbb9d668f277608224bb89122b
eelcoverbrugge <hallo@eelcoverbrugge.nl>
2023-11-01 14:13:56 +0100

    Revert reverting commits

---
+++ components/MediaPlayer/tracks.js
@@ -0,0 +1,21 @@
+import dasSpiel from "../../public/songs/Will-Hawkins-Collectief-Das-Spiel.wav";
+import howCouldYou from "../../public/songs/Will-Hawkins-Collectief-How-Could-You.wav";
+import slaveOfStone from "../../public/songs/Will-Hawkins-Collectief-Slave-Of-Stone.wav";
+
+export const tracks = [
+  {
+    title: 'How Could You',
+    src: howCouldYou,
+    author: 'Will Hawkins Collectief',
+  },
+  {
+    title: 'Das Spiel',
+    src: dasSpiel,
+    author: 'Will Hawkins Collectief',
+  },
+  {
+    title: 'Slave Of Stone',
+    src: slaveOfStone,
+    author: 'Will Hawkins Collectief',
+  },
+];
