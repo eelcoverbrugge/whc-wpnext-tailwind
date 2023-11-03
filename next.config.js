7bc1e25538002b07ce34ef610acef2eae588bc42
eelcoverbrugge <hallo@eelcoverbrugge.nl>
2023-11-03 15:20:25 +0100

    [FIX] Replace numbers to mp3

--- next.config.js
+++ next.config.js
@@ -11,7 +11,7 @@ const nextConfig = {
   },
   webpack: (config) => {
     config.module.rules.push({
-      test: /\.wav$/,
+      test: /\.mp3$/,
       use: {
         loader: 'file-loader',
         options: {
