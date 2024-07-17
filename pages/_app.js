5c58a78362751aeb5f29f5d79bd6805bfa419781
eelcoverbrugge <hallo@eelcoverbrugge.nl>
2024-07-17 16:01:50 +0200

    [FEAT]: add pathname

--- pages/_app.js
+++ pages/_app.js
@@ -2,26 +2,20 @@ import React from "react";
 import "../styles/globals.css";
 import {ParallaxProvider} from "react-scroll-parallax";
 import { GoogleAnalytics } from "nextjs-google-analytics";
-import { useEffect } from "react";
 
 function MyApp({ Component, pageProps }) {
-  const [currHostName, setCurrentHostname] = React.useState("");
-
-  useEffect(() => {
-    if (currHostName === "") {
-      setCurrentHostname(document.location.hostname);
-    }
-  }, [currHostName, setCurrentHostname]);
-
-  useEffect(() => {
-    if (currHostName !== "") {
-      console.log("currHostName: ", currHostName)
-    }
-  }, [currHostName]);
-
   return (
     <div className="font-body">
+      {document.location.hostname === "staging.willhawkinscollectief.nl" && (
+        <div className="w-full h-10 bg-amber-700 p-4">
+          <div className="text-white font-bold">
+            Let op: U bevindt zich op de <strong>staging</strong> versie van deze website.
+          </div>
+        </div>
+      )}
+
       <GoogleAnalytics trackPageViews gaMeasurementId={"G-X63KM0K59M"} />
+
       <ParallaxProvider scrollAxis={"vertical"}>
         <Component {...pageProps} />
       </ParallaxProvider>
