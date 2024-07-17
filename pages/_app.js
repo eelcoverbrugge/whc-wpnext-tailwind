3a35ef4888cfe6772a4dc6f125aa59f73ab3cc27
eelcoverbrugge <hallo@eelcoverbrugge.nl>
2024-07-17 15:48:28 +0200

    [FEAT]: add pathname

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,11 @@
 import "../styles/globals.css";
 import {ParallaxProvider} from "react-scroll-parallax";
 import { GoogleAnalytics } from "nextjs-google-analytics";
+import { usePathname } from 'next/navigation';
 
 function MyApp({ Component, pageProps }) {
+  const pathname = usePathname();
+  console.log("pathname: ", pathname);
   return (
     <div className="font-body">
       <GoogleAnalytics trackPageViews gaMeasurementId={"G-X63KM0K59M"} />
